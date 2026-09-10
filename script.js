c084601d9fa2c905cdce46064616a0e1514e009d
João Pedro <jpsoaress999@gmail.com>
2026-09-10 20:02:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+// ========================================
+// BOTÃO "ENTRE EM CONTATO"
+// ========================================
+
 const botaoContato = document.getElementById("botao-contato");
 const contato = document.getElementById("contato");
 
 botaoContato.addEventListener("click", function () {
+
     contato.scrollIntoView({
-        behavior: "smooth"
+        behavior: "smooth",
+        block: "start"
     });
+
 });
